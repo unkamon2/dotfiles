335f3a46e47d88ed88aa66e0ac1a18479d732f7d
Dylan <dvuz@offsitedatasync.com>
2015-06-08 16:48:51 -0400

    made setup easier

--- setup.sh
+++ setup.sh
@@ -5,6 +5,11 @@ git submodule update
 
 dotfile_location=`pwd`
 
+rm $HOME/.zshrc
+rm $HOME/.aliases
+rm $HOME/.functions
+rm $HOME/.oh-my-zsh/themes/custom.zsh-theme
+
 ln -s $dotfile_location/aliases $HOME/.aliases
 ln -s $dotfile_location/functions $HOME/.functions
 ln -s $dotfile_location/zshrc $HOME/.zshrc
