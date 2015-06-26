b0e924a2d76e8a0061a2c3f663f3caa7be99474d
Dylan <dvuz@offsitedatasync.com>
2015-06-26 08:49:25 -0400

    fixed setup

--- setup.sh
+++ setup.sh
@@ -10,9 +10,13 @@ rm $HOME/.aliases
 rm $HOME/.functions
 rm $HOME/.oh-my-zsh/themes/custom.zsh-theme
 rm $HOME/.tmux.conf
+rm -rf $HOME/.oh-my-zsh
+rm -rf $HOME/.tmuxifier
 
 ln -s $dotfile_location/aliases $HOME/.aliases
 ln -s $dotfile_location/functions $HOME/.functions
+ln -s $dotfile_location/oh-my-zsh $HOME/.oh-my-zsh
 ln -s $dotfile_location/zshrc $HOME/.zshrc
 ln -s $dotfile_location/custom $HOME/.oh-my-zsh/themes/custom.zsh-theme
 ln -s $dotfile_location/tmux.conf $HOME/.tmux.conf
+ln -s $dotfile_location/tmuxifier $HOME/.tmuxifier
