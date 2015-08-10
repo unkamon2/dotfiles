d353f10d69656f0d3b11c828964751beb3919cf1
Dylan Vuz <vuzdylan@gmail.com>
2015-08-09 22:14:06 -0400

    added a clean up script

---
+++ clean.sh
@@ -0,0 +1,8 @@
+rm $HOME/.aliases
+rm $HOME/.functions
+rm $HOME/.oh-my-zsh
+rm $HOME/.zshrc
+rm $HOME/.vimrc
+rm $HOME/.tmux.conf
+rm $HOME/.vim
+
--- setup.sh
+++ setup.sh
@@ -12,4 +12,3 @@ ln -s $dotfile_location/zshrc $HOME/.zshrc
 ln -s $dotfile_location/vimrc $HOME/.vimrc
 ln -s $dotfile_location/tmux.conf $HOME/.tmux.conf
 ln -s $dotfile_location/vim $HOME/.vim
-ln -s $dotfile_location/vimrc.bundle $HOME/.vimrc.bundle
